119aec8251e72b1622c0c8ff98046e19ab9eb0ac
Home <home@Homes-iMac.local>
2022-08-25 21:23:21 -0400

    Blueprint update

--- ProgramUI.cs
+++ ProgramUI.cs
@@ -1,17 +1,39 @@
 public class ProgramUI
 {
-
-    public void Run()
+        static char[] boardArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1; 
+        static int startPoint; //Where the player wants to start the game
+        static int statusCheck = 0; //Checks if player is winning, is tied, or is still playing. 
+    public void Run(string[] args)
     {
-        // Tic Tac Toe board:
-        System.Console.WriteLine("     |         |");
-        System.Console.WriteLine("     |         |");
-        System.Console.WriteLine("_____|_________|_____");
-        System.Console.WriteLine("     |         |");
-        System.Console.WriteLine("     |         |");
-        System.Console.WriteLine("_____|_________|_____");
-        System.Console.WriteLine("     |         |");
-        System.Console.WriteLine("     |         |");
+        // do {
+                // Choosing player turn
+                // Where to place and if place is already full, reloading board so player can choose again.
+        // }
+        // while {
+                // Call WinStatus to constantly check if a player has won or drawn.
+        // }
     }
 
-}
+
+    private static void Board()
+        {
+            // Tic Tac Toe board:
+            System.Console.WriteLine("     |         |");
+            System.Console.WriteLine("     |         |");
+            System.Console.WriteLine("_____|_________|_____");
+            System.Console.WriteLine("     |         |");
+            System.Console.WriteLine("     |         |");
+            System.Console.WriteLine("_____|_________|_____");
+            System.Console.WriteLine("     |         |");
+            System.Console.WriteLine("     |         |");
+        }
+
+    private static int WinStatus()
+    {
+        // Check for a win in each column and row
+        // Check for a draw
+
+    }
+
+    }
